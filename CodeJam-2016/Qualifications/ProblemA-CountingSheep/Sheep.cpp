550a5ee69653398bf7c3d4f244a75fcd79c21a2c
DasAbhi <abhirup.das@hotmail.com>
2016-06-26 20:09:41 -0400

    added function for iterating through number

--- CodeJam-2016/Qualifications/ProblemA-CountingSheep/Sheep.cpp
+++ CodeJam-2016/Qualifications/ProblemA-CountingSheep/Sheep.cpp
@@ -4,15 +4,39 @@
 
 using namespace std;
 
+int IntegerIterator(int Number){
+  bool DigitHolder [10];
+
+  //have to initialize the array with zeros
+  for (int i = 0; i < 10; i++){
+    DigitHolder[i] = 0;
+  }
+
+  int DigitCounter = 0;
+  int iterator;
+  bool DigitBool = true;
+
+  string ConvertedSringNumber = to_string(Number);
+
+  while (DigitBool){
+    if (DigitCounter == 10){
+      DigitBool = false;
+    }
+    else {
+
+    }
+  }
+}
+
+
 int main(){
-
-
-  //string s = to_string(24);
+  string s = to_string(24);
 
   ifstream fin("input.txt");
   ofstream fout("output.txt");
 
-  int InputCounter, GarbagePlaceHolder, InputInteger;
+  int InputCounter, InputInteger, FinalInteger;
+  string GarbagePlaceHolder;
 
   fout << "Output" << endl;
   fin >> GarbagePlaceHolder >> InputCounter;
@@ -23,7 +47,12 @@ int main(){
       //insomnia
       fout << "INSOMNIA" << endl;
     }
+    else {
+      FinalInteger = IntegerIterator(InputInteger);
+      fout << FinalInteger << endl;
+    }
   }
 
   fin.close();
+  fout.close();
 }
